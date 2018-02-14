ec0c03172809de479fffa41d4a9cbd4958bc497c
Antoine Balestrat <antoine.balestrat@gmail.com>
2018-02-14 10:12:06 +0100

    Added TODOs & FIXMEs Cleanups

--- rtl.ml
+++ rtl.ml
@@ -8,30 +8,6 @@ let locenv : ((Ttree.decl_var, register) Hashtbl.t) = Hashtbl.create 1
 
 let generate i = let l = Label.fresh () in graph := Label.M.add l i !graph; l
 
-(*
-	    	type binop = | Beq | Bneq | Blt | Ble | Bgt | Bge |
-	    	Badd | Bsub | Bmul | Bdiv | Band | Bor -> fait
-    *)
-    (*
-	    Embinop of mbinop * register * register * label
-	    attention au sens : [op r1 r2] représente [r2 <- r2 op r1]
-	*)
-
-	(*
-	type mbinop =
-  | Mmov
-  | Madd
-  | Msub
-  | Mmul
-  | Mdiv
-  | Msete
-  | Msetne
-  | Msetl
-  | Msetle
-  | Msetg
-  | Msetge
-	*)
-
 let binop2Op bnop = match bnop with
 	| Ptree.Beq  -> Ops.Msete
 	| Ptree.Bneq -> Ops.Msetne
@@ -41,6 +17,7 @@ let binop2Op bnop = match bnop with
 	| Ptree.Bge  -> Ops.Msetge
 	| _			 -> failwith "Unreachable binop2Op"
 
+(* Ça peut se mémoïser ça non ? Changer la datastructure "structure" pour stocker une Hashtbl var -> index plutôt que la liste ordonnée pour obtenir l'index en O(1) *)
 let indexInList e l =
 	let rec aux i ll = match ll with
 		| []   -> i
@@ -77,15 +54,21 @@ let rec condition e truel falsel =
 	and expr (e : Ttree.expr) destr destl = match e.Ttree.expr_node with
 		| Ttree.Econst n -> generate (Econst (n, destr, destl))
 		| Ttree.Eaccess_local nomVar ->  let regVar = Hashtbl.find locenv (e.Ttree.expr_typ, nomVar) in generate (Embinop (Ops.Mmov, regVar, destr, destl))
+
+		(* Attention: ici on génère un simple mov de regvar vers destr, pas un load *)
 		| Ttree.Eassign_local (nomVar, expAAss) -> 	let regVar = Hashtbl.find locenv (e.Ttree.expr_typ, nomVar) in
 					                               	let labelAss = generate (Embinop (Ops.Mmov, destr, regVar, destl))in
 					                            	expr expAAss destr labelAss
+
 		| Ttree.Ebinop _ -> generateBinop e.Ttree.expr_node destr destl
 		| Ttree.Eunop  _ -> generateUnop e.Ttree.expr_node destr destl
 		| Ttree.Eaccess_field (ex, f)     -> (match ex.Ttree.expr_typ with
 												| Ttree.Tstructp s -> (let n = indexInList f.Ttree.field_name s.Ttree.str_ordered_fields and rintermed = Register.fresh () in
 																	  let lres = generate (Eload (rintermed, n*8, destr, destl)) in expr ex rintermed lres)
 												| _                -> failwith "Unreachable type")
+
+		(* Si c'est u->a = 42 (où u est un access local, premier cas matché), on veut store 42 dans +n(u) donc on va chercher le registre de u *)
+		(* Si c'est u->u->a, on veut d'abord loader le u de u (récursivement, d'où le expr e1 ...) pour ensuite faire le store *)
 		| Ttree.Eassign_field (e1, f, e2) -> (match (e1.Ttree.expr_typ, e1.Ttree.expr_node) with
 												| (Ttree.Tstructp s, Ttree.Eaccess_local i)  -> (let regVar = Hashtbl.find locenv (e1.Ttree.expr_typ, i) in
 																								 let n = indexInList f.Ttree.field_name s.Ttree.str_ordered_fields in
@@ -95,8 +78,12 @@ let rec condition e truel falsel =
 																		    let lres = generate (Estore (destr, rinter, n*8, destl)) in
 																		    let l1 = expr e1 rinter lres in expr e2 destr l1)
 												| _                     -> failwith "Unreachable type")
+
+		(* C'est très simple puisque on sait que tous les champs de structure font 8 bytes *)
 		| Ttree.Esizeof s -> generate (Econst (Int32.of_int (8 * (List.length s.Ttree.str_ordered_fields)), destr, destl))
-		(* Ecall of register * string * register list * label *)
+
+		(* "Fold right 2" permet de consommer 2 listes de la droite vers la gauche en même temps ! 
+	       Pratique pour parcourir en même temps les expressions passées en paramètres et les registres dans lesquels on les met ! *)
 		| Ttree.Ecall (fid, exprlist) -> let myregs = List.map (fun x -> Register.fresh ()) exprlist in
 										 let lres = generate (Ecall (destr, fid, myregs, destl)) in
 										 List.fold_right2 (fun arg reg lab -> expr arg reg lab) exprlist myregs lres
@@ -105,6 +92,7 @@ let rec condition e truel falsel =
 		| Ttree.Sreturn e -> expr e retr exitl
 		| Ttree.Sexpr e   -> let unusedReg = Register.fresh() in expr e unusedReg destl
 		| Ttree.Sskip     -> destl
+
 		(* Idiome repris de "fct" *)
 		| Ttree.Sblock (decvarl, stmtl) -> (let rec populate l = (match l with
     											| [] -> []
@@ -112,43 +100,21 @@ let rec condition e truel falsel =
     									   let _ = populate decvarl in
     									   let l1 = List.fold_right (fun stm labelnext -> stmt stm labelnext retr exitl) stmtl destl in
     									   List.iter  (fun v -> Hashtbl.remove locenv v) decvarl; l1)
+
+		(* FIXME: utiliser ta fonction condition qui fait mieux le taf (même si avec ça, ça fonctionne) ! *)
 		| Ttree.Sif (e, strue, sfalse)  -> let lfalse = stmt sfalse destl retr exitl and ltrue = stmt strue destl retr exitl in
 										   let rintermed = Register.fresh () in
 										   let comparee = generate (Emubranch (Ops.Mjz, rintermed, lfalse, ltrue)) in
 										   let computee = expr e rintermed comparee in
 										   computee
+		(* TODO: implémenter le dernier statement : while *)
 		| _ -> failwith "unimplemented stmt"
-    (*
-	    	type binop = | Beq | Bneq | Blt | Ble | Bgt | Bge |
-	    	Badd | Bsub | Bmul | Bdiv | Band | Bor -> fait
-    *)
-    (*
-	    Embinop of mbinop * register * register * label
-	    attention au sens : [op r1 r2] représente [r2 <- r2 op r1]
-	*)
-
-	(*
-	type mbinop =
-  | Mmov
-  | Madd
-  | Msub
-  | Mmul
-  | Mdiv
-  | Msete
-  | Msetne
-  | Msetl
-  | Msetle
-  | Msetg
-  | Msetge
-	*)
-
 
     and generateBinop e destr destl = match e with
     	| Ttree.Ebinop (op, e1, e2) when (List.mem op [Ptree.Badd; Ptree.Bsub; Ptree.Bmul; Ptree.Bdiv]) ->  
     											let r2 = Register.fresh () in let lres = generate (Embinop (ptreeOp2Mbinop op, r2, destr, destl)) in
     											let l2 = expr e2 r2 lres in
     											let l1 = expr e1 destr l2 in l1
-    	(* FIXME: vérifier que c'est bien le bon ordre dans les paramètres de mubranch *)
     	| Ttree.Ebinop (Ptree.Bor, e1, e2) -> (let rintermed = Register.fresh () in 
     										  let set1 = generate (Econst (Int32.of_int 1, destr, destl)) and set0 = generate (Econst (Int32.of_int 0, destr, destl)) in
     										  let compare2 = generate (Emubranch (Ops.Mjz, rintermed, set0, set1)) in
@@ -167,42 +133,18 @@ let rec condition e truel falsel =
     										   let lres = generate (Embinop (binop2Op op, rintermed, destr, destl)) in
     										   let l2 = expr e2 rintermed lres in let l1 = expr e1 destr l2 in
     										   l1
-    	| _ -> failwith "Unimplemented binop"
+    	| _ -> failwith "Unreachable generateBinop"
 
-(* type expr = {
-  expr_node: expr_node;
-  expr_typ : typ        (* chaque expression est décorée par son type *)
-}*)
     and generateUnop e destr destl = match e with
     	| Ttree.Eunop (Ptree.Unot, e)  -> let lres = generate (Emunop ((Ops.Msetei (Int32.of_int 0)), destr, destl)) in let le = expr e destr lres in le 
     	| Ttree.Eunop (Ptree.Uminus, e) -> generateBinop (Ttree.Ebinop (Ptree.Bsub, {Ttree.expr_node = Ttree.Econst (Int32.of_int 0); Ttree.expr_typ = Ttree.Tint}, e)) destr destl
     	| _ -> failwith "nope"
 
-(*and decl_fun = {
-  fun_typ    : typ;
-  fun_name   : ident;
-  fun_formals: decl_var list;
-  fun_body   : block
-}
-*)
-
-(*
-type deffun = {
-  	fun_name : string;
-  	fun_formals : register list;
-  	fun_result : register;
-  	fun_locals : Register.set;	(*toutes les variables locales de la fonction maintenant regroupées ici *)
-  	fun_entry : label;
-  	fun_exit : label;
-  	fun_body : cfg;
-}
-*)
-    let fct (f : Ttree.decl_fun) =
-    							let rec populate (l : Ttree.decl_var list) = match l with
-    																	| [] -> []
-    																	| x::ll -> let nreg = Register.fresh () in
-    																				Hashtbl.add locenv x nreg;
-    																				(nreg)::populate ll in
+    let fct (f : Ttree.decl_fun) = let rec populate (l : Ttree.decl_var list) = match l with
+    									| [] -> []
+    									| x::ll -> let nreg = Register.fresh () in
+    											   Hashtbl.add locenv x nreg;
+    											   (nreg)::populate ll in
     							let fformals  = populate f.Ttree.fun_formals in
     							let fres      = Register.fresh () in 
     							let flocals   = Register.set_of_list (populate (fst f.Ttree.fun_body)) in
