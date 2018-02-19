41cd18d290c4397ec502f4b4f7179995f98cfd18
ankprd <foo@gmail.com>
2018-02-19 16:24:31 +0100

    dshakj

--- ertl.ml
+++ ertl.ml
@@ -5,14 +5,14 @@ let graphERTL = ref (Label.M.empty : instr Label.M.t)
 let instr curLabel curInstr = failwith "TODO"
 
 let fct (f: Rtltree.deffun) = 
-    Hashtbl.clear graphERTL;
-    Hashtbl.iter instr f.Rtltree.fun_body; 
+    graphERTL := (Label.M.empty : instr Label.M.t);
+    Label.M.iter instr f.Rtltree.fun_body; 
     {
-        fun_name : f.Rtltree.fun_name;
-        fun_formals : List.length f.Rtltree.fun_formals; (* nb total d'arguments *)
-        fun_locals : f.Rtltree.fun_locals;
-        fun_entry : f.Rtltree.fun_entry;
-        fun_body : !graphERTL;
+        fun_name = f.Rtltree.fun_name;
+        fun_formals = List.length f.Rtltree.fun_formals; (* nb total d'arguments *)
+        fun_locals = f.Rtltree.fun_locals;
+        fun_entry = f.Rtltree.fun_entry;
+        fun_body = !graphERTL;
     }
 
 let program p = 
