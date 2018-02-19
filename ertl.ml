a3ac9b3086cad30e3f6dd04d261b44777e4f4bc3
ankprd <foo@gmail.com>
2018-02-19 16:18:15 +0100

    ertl.ml

---
+++ ertl.ml
@@ -0,0 +1,22 @@
+open Ertltree
+
+let graphERTL = ref (Label.M.empty : instr Label.M.t)
+
+let instr curLabel curInstr = failwith "TODO"
+
+let fct (f: Rtltree.deffun) = 
+    Hashtbl.clear graphERTL;
+    Hashtbl.iter instr f.Rtltree.fun_body; 
+    {
+        fun_name : f.Rtltree.fun_name;
+        fun_formals : List.length f.Rtltree.fun_formals; (* nb total d'arguments *)
+        fun_locals : f.Rtltree.fun_locals;
+        fun_entry : f.Rtltree.fun_entry;
+        fun_body : !graphERTL;
+    }
+
+let program p = 
+    let rec aux pl = match pl with
+      | [] -> []
+      | f::l -> (fct f)::(aux l) in
+      {funs = (aux p.Rtltree.funs)} 
