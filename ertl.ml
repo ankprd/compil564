0d2906aa7dae962e1b7eaf4a13354bf024916f7e
ankprd <foo@gmail.com>
2018-02-19 17:38:54 +0100

    Entry in function

--- ertl.ml
+++ ertl.ml
@@ -18,7 +18,36 @@ let instr curLabel curInstr =
 
 let fct (f: Rtltree.deffun) = 
     graphERTL := (Label.M.empty : instr Label.M.t);
+    (*Instructions d'entree dans une fct*)
+    let rec getParams funForm regList offset = 
+        match (funForm, regList) with
+        |([], _) -> f.Rtltree.fun_entry (*tous les parametres sont dans les reg, on peut entrer dans la fct*)
+        |(rf::f, []) -> 
+            let labelSuiv = getParams f [] (offset + 8) in 
+            let curLabel = Label.fresh () in
+            addToGraph curLabel (Eget_param(offset, rf, labelSuiv)); curLabel
+        |(rf::f, rr::l) -> 
+            let labelSuiv = getParams f l offset in
+            let curLabel = Label.fresh () in
+            addToGraph curLabel (Embinop(Mmov, rr, rf, labelSuiv)); curLabel (*mov source, dest*)
+    in
+    let labelBeforeGetParams = getParams f.Rtltree.fun_formals Register.parameters 16
+    in
+    let rec savesCalleeReg regList =
+        match regList with
+        |[] -> labelBeforeGetParams
+        |r::t -> 
+            let labelSuiv = savesCalleeReg t in
+            let curLabel = Label.fresh () in
+            addToGraph curLabel (Embinop(Mmov, r, Register.fresh (), labelSuiv)); curLabel
+    in 
+    let labelBeforeCalleeSaving  = savesCalleeReg Register.callee_saved in
+    let labelBeginFct = Label.fresh () in
+    addToGraph labelBeginFct (Ealloc_frame labelBeforeCalleeSaving);
+
     Label.M.iter instr f.Rtltree.fun_body; 
+    
+    addToGraph f.Rtltree.fun_exit Ereturn;
     {
         fun_name = f.Rtltree.fun_name;
         fun_formals = List.length f.Rtltree.fun_formals; (* nb total d'arguments *)
--- test.c
+++ test.c
@@ -1,6 +1,3 @@
 int main() {
-  putchar(66 + -1);
-  putchar(65 + -(-1));
-  putchar(10);
   return 0;
 }
