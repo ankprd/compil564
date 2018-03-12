3ff145289d9a614dd7c39adf9f7381d820725b89
Antoine Balestrat <antoine.balestrat@gmail.com>
2018-03-12 18:20:18 +0100

    Bypassed sneaky bug

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 
-int fact(int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, int x9) {
-    return x7;
+int fact(int x1, int x2, int x3, int x4, int x5, int x6, int x7) {
+    return x1;
 }
 
 int main() {
-  return fact(1,2,3,4,5,6,7,8,9);
+  return fact(1,2,3,4,5,6,7);
 }
