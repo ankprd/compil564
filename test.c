9e72810d89fa5b95973c377b5e9cebab4d620b9b
ankprd <foo@gmail.com>
2018-03-17 15:52:38 +0100

    test

--- test.c
+++ test.c
@@ -1,13 +1,3 @@
-
-struct U { int x; int y; };
-
-struct S { int a; struct U *u; int b; };
-
-int main() {
-  struct S *s;
-
-  s = sbrk(sizeof(struct S));
-  s->u = sbrk(sizeof(struct U));
-  
-  return 0;
+int main(){
+  putchar(-593/47);
 }
