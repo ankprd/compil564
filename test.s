732fb0b72435fc9c791ec53cb9cd47fe01b48d3e
ankprd <foo@gmail.com>
2018-03-17 12:27:12 +0100

    segfault : example in test.c

--- test.s
+++ test.s
@@ -2,33 +2,21 @@
 	.globl	main
 
 main:
-L13:
-	pushq %rbp
-L25:
-	movq %rsp, %rbp
-L24:
-	addq $-8, %rsp
 L12:
 L11:
 L10:
-	movq $24, %rdi
 L9:
-L19:
-L18:
-	call sbrk
-L17:
-L8:
-	movq %rax, -8(%rsp)
-L7:
 	movq $16, %rdi
-L6:
-L16:
+L8:
 L15:
-	call sbrk
 L14:
+	call sbrk
+L13:
+L7:
+L6:
+	movq $3, %r10
 L5:
-	movq -8(%rsp), %r10
 L4:
 L3:
-	movq %rax, 8(%r10)
+	movq %r10, 0(%rax)
 	.data
